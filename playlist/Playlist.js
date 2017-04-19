30e70b7c5be6d8bc44dbde02ad8d73e7d5bff6ec
Lucas Burdeyron <lucas.burdeyron@gmail.com>
2017-04-19 15:32:07 +0200

    fix bug when playnext

--- playlist/Playlist.js
+++ playlist/Playlist.js
@@ -1,56 +1,57 @@
 var os = require('os');
 
 function Playlist(player) {
-//  console.log('Create Playlist');
+    //  console.log('Create Playlist');
     this.player = player;
     this.queue = [];
     this.playing = undefined;
 }
 
 Playlist.prototype.addToQueue = function (playlist_item, cb) {
-  this.queue.push(playlist_item);
-  if(this.queue.length == 1){
-    this.playNext();
-  } else {
-    playlist_item.sound.downloadFile();
-  }
+    this.queue.push(playlist_item);
+    if(this.queue.length == 1){
+        this.playNext();
+    } else {
+        playlist_item.sound.downloadFile();
+    }
 }
 
 Playlist.prototype.playNext = function(force){
-  if('undefined' === typeof force){
-    force=false;
-  }
-  if(force){
-    this.queue.shift();
-    var isWin = /^win/.test(os.platform());
-    if(!isWin) {
-        this.player.audio.kill('SIGKILL');
-    } else {
-        var cp = require('child_process');
-        cp.exec('taskkill /PID ' + this.player.audio.pid + ' /T /F', function (error, stdout, stderr) {});
+    if('undefined' === typeof force){
+        force=false;
     }
-    this.player.status = 'offair'
-  }
-  var playlist = this;
-  var next = this.queue[0];
-  this.playing = undefined;
-  if('undefined' !== typeof next){
-    if(next.like >= next.dislike){
-      this.playing = next;
-      this.player.playSound(next.sound, function(){
-        playlist.queue.shift();
-        next.sound.deleteFile();
-        if('undefined' !== next.endPlaying){
-          next.endPlaying()
+    if(force){
+        this.queue.shift();
+        var isWin = /^win/.test(os.platform());
+        if(!isWin) {
+            this.player.audio.kill('SIGKILL');
+        } else {
+            var cp = require('child_process');
+            cp.exec('taskkill /PID ' + this.player.audio.pid + ' /T /F', function (error, stdout, stderr) {});
         }
-        playlist.playNext();
-      });
+        this.player.status = 'offair'
     } else {
-      this.queue.shift();
-      next.sound.deleteFile();
-      this.playNext();
+        var playlist = this;
+        var next = this.queue[0];
+        this.playing = undefined;
+        if('undefined' !== typeof next){
+            if(next.like >= next.dislike){
+                this.playing = next;
+                this.player.playSound(next.sound, function(){
+                    playlist.queue.shift();
+                    next.sound.deleteFile();
+                    if('undefined' !== next.endPlaying){
+                        next.endPlaying()
+                    }
+                    playlist.playNext();
+                });
+            } else {
+                this.queue.shift();
+                next.sound.deleteFile();
+                this.playNext();
+            }
+        }
     }
-  }
 }
 
 module.exports = Playlist;
