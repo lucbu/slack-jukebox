42afb282acd5ce965e5bcbe7fa785641a3527a5d
JRK <JRK>
2018-04-27 12:40:22 +0200

    l

--- sound/YoutubeSound.js
+++ sound/YoutubeSound.js
@@ -78,30 +78,30 @@ YoutubeSound.prototype.downloadFile = function(cb) {
          }
     });
 
-    /*
-    this.downloading = ytdl(this.url, this.ytdlOpts);
+    if (this.timeRange == null) {
+        this.downloading = ytdl(this.url, this.ytdlOpts);
 
-    this.downloading
-        .pipe(fs.createWriteStream(file))
-        .on('finish', function(test) {
-            console.log('### End downloading ' + file);
-        })
-    ;
-    */
-
-    horizon.downloadToLocal(this.url, this.mountpoint, this.basename, this.timeRange, null, function(err, complete) {
-        if (err) {
-            console.log('Error during download', err);
-        }
-
-        console.log('### End downloading ' + file);
-    }, function(percent, timemark, targetSize){
-        // console.log('Progress:', percent, 'Timemark:', timemark, 'Target Size:', targetSize);
-        // Will return...
-        // Progress: 90.45518257038955 Timemark: 00:02:20.04 Target Size: 2189
-        // Progress: 93.73001672942894 Timemark: 00:02:25.11 Target Size: 2268
-        // Progress: 100.0083970106642 Timemark: 00:02:34.83 Target Size: 2420
-    });
+        this.downloading
+            .pipe(fs.createWriteStream(file))
+            .on('finish', function(test) {
+                console.log('### End downloading ' + file);
+            })
+        ;
+    } else {
+        horizon.downloadToLocal(this.url, this.mountpoint, this.basename, this.timeRange, null, function(err, complete) {
+            if (err) {
+                console.log('Error during download', err);
+            } else {
+                console.log('### End downloading ' + file);
+            }
+        }, function(percent, timemark, targetSize){
+            // console.log('Progress:', percent, 'Timemark:', timemark, 'Target Size:', targetSize);
+            // Will return...
+            // Progress: 90.45518257038955 Timemark: 00:02:20.04 Target Size: 2189
+            // Progress: 93.73001672942894 Timemark: 00:02:25.11 Target Size: 2268
+            // Progress: 100.0083970106642 Timemark: 00:02:34.83 Target Size: 2420
+        });
+    }
 
     if ('undefined' !== typeof cb) {
         setTimeout(cb, 5000);
