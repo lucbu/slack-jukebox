9dba2dad26b100516250900c8e5f2739ac8b6895
root <root@jrk.pi>
2018-05-02 10:22:26 +0200

    additionnal listeners for player

--- player/Player.js
+++ player/Player.js
@@ -23,6 +23,10 @@ Player.prototype.playSound = function(sound, cb) {
 
             player.audio = spawn(VLC_PATH, [sound.filename, '--intf' ,'rc', '--play-and-exit']);
 
+	    player.audio.on('error', function(err) { console.log('*Error'); console.log(err); });
+            player.audio.on('close', function(close) { console.log('*Close'); console.log(close); });
+	    player.audio.on('disconnect', function(disconnect) { console.log('*Disconnect'); console.log(disconnect); });
+
             player.audio.on('exit', function(code) {
                 console.log('End music ' + sound.filename);
 
